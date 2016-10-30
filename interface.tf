c1ce5bfcb98b782ae65d9bf697eb032f42077818
Peter Miron <peter.miron@apcera.com>
2016-10-30 11:20:45 -0400

    initial update of tf_remote_state bucket.

---
+++ interface.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "us-east-1" 
+  description = "The AWS region."
+}
+variable "environment" {
+  default = "development"
+  description = "The name of our environment, i.e. development."
+}
+
---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "remote_state" { 
+     bucket = "pjm_remote_state-${var.environment}" 
+     acl = "authenticated-read"
+     versioning {
+       enabled = true
+     }
+     lifecycle { 
+           prevent_destroy = true
+     }
+     tags {
+       Name = "remote_state-${var.environment}"
+       Environment = "${var.environment}"
+     } 
+}
